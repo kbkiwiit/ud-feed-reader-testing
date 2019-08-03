a6d288a746c5d34797c62002223943ea6e57597f
Kurt Bellian <kelliano@gmail.com>
2019-08-03 16:34:52 +1200

    Created all test suites and menu unit tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,18 +1,16 @@
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
+        /* Test that the allFeeds variable has been populated
+         * with feeds and is not empty.
+         */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* Loop through all feeds and ensujre a URL has been
+         * defined and that URL is not empty.
          */
         it('has URL', function() {
             for(let feed of allFeeds) {
@@ -23,9 +21,8 @@ $(function() {
             
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* Loop through all feeds and ensujre a name has been
+         * defined and that name is not empty.
          */
         it('has name', function() {
             for(let feed of allFeeds) {
@@ -37,36 +34,49 @@ $(function() {
         });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function() {
-
-    });
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+    describe('The menu', function() { 
+        /* Test that the slide menu is hidden by default when page
+         * loads.
          */
+        it('menu hidden', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* Test if the slide menu is visible or hidden when the
+          * menu icon is clicked.
           */
+         it('menu visible or hidden when clicked', function() {
+             let menu = document.querySelector('a.menu-icon-link');
+             menu.click();
+             expect($('body').hasClass('menu-hidden')).toBe(false);
+             menu.click();
+             expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
+    });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
 
+    describe('Initial Entries', function() { 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('menu hidden', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    
 
+    describe('New Feed Selection', function() { 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('menu hidden', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
+    });
+
 }());
